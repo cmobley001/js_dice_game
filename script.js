06e81d0605e7934f495ac9a8c80189ab6bf0618b
cmobley001 <cmobley001@student.butte.edu>
2024-09-02 06:30:13 -0700

    updated files

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-'use strict';
+"use strict";
+
+//get app elements
+const id_score0 = document.querySelector("#score--0");
+const id_score1 = document.getElementById("score--1");
+const img_dice = document.querySelector(".dice");
+const btnNewGame = document.querySelector("btn--new");
+const btnRollDice = document.querySelector("btn--roll");
+const btnHold = document.querySelector("btn--hold");
+
+id_score0.textContent = 0;
+id_score1.textContent = 0;
+img_dice.classList.add("hidden");
+
+//rolling the dice
